669f2702f36ef8fdfe78530b9a1e09bf1362a48f
Ola Baut-Kulec <olabaut@gmail.com>
2023-01-04 22:15:19 +0100

    add Column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,5 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
 
 const List = () => {
     return (
@@ -6,9 +7,9 @@ const List = () => {
             <header className={styles.header}><h2 className={styles.title}>Things to do<span>soon</span></h2></header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}> 
-                <article><h2>Books</h2></article>
-                <article><h2>Movies</h2></article>
-                <article><h2>Games</h2></article>
+               <Column title='Books'/>
+               <Column title='Movies'/>
+               <Column title='Games'/>
             </section>
         </div>
     );
